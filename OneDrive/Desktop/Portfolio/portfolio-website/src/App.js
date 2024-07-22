0353381140471dc2c7a6c43f17a8602fa3fe7ecc
ekassz <ejrobles@wisc.edu>
2024-07-22 01:09:19 -0500

    updated UI & navbar

--- OneDrive/Desktop/Portfolio/portfolio-website/src/App.js
+++ OneDrive/Desktop/Portfolio/portfolio-website/src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './Pages/Home/HomeScreen';
-
+import HeroSection from './Pages/Home/HeroSection';
+import AboutMe from './Pages/Home/AboutMe';
+import Resume from './Pages/Home/Resume';
+import Projects from './Pages/Home/Projects';
+import ContactMe from './Pages/Home/ContactMe';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Home/>
+      <Routes>
+        <Route path='/home' element={<HeroSection/>}/>
+        <Route path='/aboutme' element={<AboutMe/>}/>
+        <Route path='/resume' element={<Resume/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+        <Route path='/contact' element={<ContactMe/>}/>
+      </Routes>
 
     </div>
+    </Router>
   );
 }
 
--- OneDrive/Desktop/Portfolio/portfolio-website/src/Pages/Home/HeroSection.jsx
+++ OneDrive/Desktop/Portfolio/portfolio-website/src/Pages/Home/HeroSection.jsx
@@ -5,20 +5,29 @@ import React, {Component} from "react";
 export default function HeroSection() {
     return(
         <section id="heroSection" className="hero--section">
-            <div className="hero--section--content--box">
-                <div className="hero--section--content">
-                    <h2 className="section--title">
-                        Hello! Hola!
-                    </h2>
-                    <h1 className="hero--section--title">
+            <nav className="navbar">
+                <ul className="nav-list">
+                    <li className="nav-item"><a href="/home">HOME</a></li>
+                    <li className="nav-item"><a href="/aboutme">ABOUT ME</a></li>
+                    <li className="nav-item"><a href="/resume">RESUME</a></li>
+                    <li className="nav-item"><a href="/projects">PROJECTS</a></li>
+                    <li className="nav-item"><a href="/contact">CONTACT</a></li>
+                </ul>
+            </nav>
+            <div className="hero--section--header">
+                <h2 className="section--title">Hello! Hola!</h2>
+                </div>
+                    <div className="hero--section--content--box">
+                        <div className="hero--section--content">
+                            <h1 className="hero--section--title">
                         <span className="hero--section--title-color">
                             I'm Emili,
                         </span>
                         <br/>
-                        an aspiring Software Engineer!
+                        an aspiring <span className="highlighted-SWE-text">Software Engineer!</span>
                     </h1>
                     <p className="hero--section--description">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsa perspiciatis aliquid est laboriosam repellat nesciunt. Facere atque sapiente magni sint molestias ad similique fugiat, natus, doloribus enim perferendis eius quia.
+                        I am a rising senior at the University of Wisconsin-Madison studying Computer Science & Data Science, with minors in Digital Studies and Leadership. 
                     </p>
                 </div>
                 <button className="btn btn-primary">Get in Touch</button>
